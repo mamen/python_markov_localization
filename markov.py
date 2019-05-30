6d0c79d37c3578fe6e9a5530e4013f2e552bc613
mamen <markusme@edu.aau.at>
2019-05-30 18:25:41 +0200

    update

--- markov.py
+++ markov.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-
 class Direction(Enum):
     Up = 1
     Right = 2
@@ -31,58 +30,114 @@ class Step():
         self.direction = direction
         self.sensedDistance = sensedDistance
 
-N = 100
+N = 8
 
-board = np.full((N, N),  1 / (N*N))
+map =  np.full((N, N), 0)
 
-realPos = Position(x=10, y=10, direction=Direction.Right)
+epsilon = 1/100000
 
-steps = [Step(direction=Direction.Right, sensedDistance=80), Step(direction=Direction.Up, sensedDistance=50), Step(direction=Direction.Up, sensedDistance=45)]
+probabilities = np.full((N, N), 1 / (N * N))
+
+realPos = Position(x=3, y=3, direction=Direction.Right)
+
+steps = [Step(direction=Direction.Right, sensedDistance=4),
+         Step(direction=Direction.Right, sensedDistance=50),
+         Step(direction=Direction.Down, sensedDistance=45),
+         Step(direction=Direction.Left, sensedDistance=45)]
 
 
-def plotMap():
-    plt.imshow(board, cmap='hot')
+def plotMap(map):
+
+    # plot correlation matrix
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(map, vmin=0, vmax=np.max(map))
+    fig.colorbar(cax)
+    ticks = np.arange(0, N, 1)
+
+    ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
+
+    ax.set_xticklabels(range(1, N))
+    ax.set_yticklabels(range(1, N))
+
     plt.show()
 
 
 def calcPrior(direction):
-    global board
+    global probabilities, epsilon
 
     # new bel at position x = sum over all old positions x { prob(new position x | sensordata now, pos old) * believe(old position)
 
-    stepSize = getStepSize()
-    weights = [0.1, 0.2, 0.4, 0.2, 0.1]
+    # stepSize = getStepSize()
+
+    steps = [3, 4, 5, 6, 7]
+    stepProbability = [0, 0, 0, 0.1, 0.2, 0.4, 0.2, 0.1]
 
     p = np.full((N, N),  1)
+
+    newProbabilities = np.full((N, N), 0.0)
+
+    for i in range(0, N):
+        for j in range(0, N):
+            for stepSize in steps:
+                if j + stepSize < N:
+                    newProbabilities[i, j + stepSize] += probabilities[i, j] * stepProbability[stepSize]
+
+    newProbabilities[newProbabilities < epsilon] = epsilon
+
+    return newProbabilities
+
+
+def calcPosterior(sensorValue, direction):
+    global probabilities, epsilon
+
+    sensorValues = [sensorValue - 2, sensorValue - 1, sensorValue, sensorValue + 1, sensorValue + 2]
+
+    sensorProbability = [0.1, 0.2, 0.4, 0.2, 0.1]
+
 
     for i in range(0, N):
         for j in range(0, N):
 
-            for k in range(1, N):
+            if direction == Direction.Up:
+                return 0
 
-                if 2 < k < 8:
-                    board[i, j + k] = board[i, j + k] * weights[k - 2]
-                else:
-                    board[i, j] = 0.0001
+            elif direction == Direction.Right:
 
+                for k in range(0, N - sensorValue - 2):
+                    probabilities[i, k] = epsilon
+
+                for k in range(sensorValue - 2 - 1, sensorValue + 2):
+                    probabilities[i, k] = probabilities[i, N - k] * sensorProbability[k - len(sensorValues) - 1]
+
+                for k in range(sensorValue + 2, N):
+                    probabilities[i, k] = epsilon
+
+            elif direction == Direction.Down:
+                return 0
+
+            elif direction == Direction.Left:
+                return 0
+
+    # 3 = 0,00001620204119
+    # 2 = 0,004957504353
+
+    print(probabilities)
     return 0
-
-
-def calcPosterior(sensorValue, direction):
-    return 0
-
-
-def getStepSize():
-    # x_t
-    population = [3, 4, 5, 6, 7]
-    weights = [0.1, 0.2, 0.4, 0.2, 0.1]
-
-    return choices(population, weights)[0]
 
 
 def getSensorDerivation():
     # z_t
     population = [-2, -1, 0, 1, 2]
+    weights = [0.1, 0.2, 0.4, 0.2, 0.1]
+
+    return choices(population, weights)[0]
+
+
+def getStepSize():
+    # x_t
+    population = [3, 4, 5, 6, 7]
     weights = [0.1, 0.2, 0.4, 0.2, 0.1]
 
     return choices(population, weights)[0]
@@ -126,25 +181,86 @@ def doStep(direction):
 
         realPos.x = realPos.x - stepSize
 
-def main():
 
-    print(realPos.x, realPos.y)
-    plotMap()
+def senseDistance(direction):
+    global realPos
+
+    distance = 0
+
+    if direction == Direction.Up:
+
+        for i in range(1, realPos.y+1):
+
+            if realPos.y - i < 0:
+                break
+
+            if map[realPos.x - i,  realPos.y] == 0:
+                distance += 1
+            else:
+                break
+
+    elif direction == Direction.Right:
+
+        for i in range(1, N - realPos.x):
+
+            if realPos.x + i > N:
+                break
+
+            if map[realPos.x,  realPos.y + i] == 0:
+                distance += 1
+            else:
+                break
+
+    elif direction == Direction.Down:
+
+        for i in range(1, N - realPos.y):
+
+            if realPos.y + i > N:
+                break
+
+            if map[realPos.x,  realPos.y + i] == 0:
+                distance += 1
+            else:
+                break
+
+    elif direction == Direction.Left:
+        for i in range(1, realPos.x + 1):
+
+            if realPos.x - i < 0:
+                break
+
+            if map[realPos.x - i, realPos.y] == 0:
+                distance += 1
+            else:
+                break
+
+    return distance
+
+
+def main():
+    global probabilities
+
+    # print(realPos.x, realPos.y)
+    plotMap(probabilities)
 
     for step in steps:
         # 1. take step
-        doStep(step.direction)
+        # doStep(step.direction)
 
         # 2. calulate prior
-        calcPrior(step.direction)
+        probabilities = calcPrior(step.direction)
+
+        # distance = senseDistance(step.direction)
+
+        distance = step.sensedDistance
 
         # 3. calulate posterior
-        calcPosterior(0, 0)
+        calcPosterior(distance, step.direction)
 
         # 4. plot new position
-        plotMap()
+        plotMap(probabilities)
 
-        print(realPos.x, realPos.y)
+        # print(realPos.x, realPos.y)
 
 
 if __name__ == "__main__":
